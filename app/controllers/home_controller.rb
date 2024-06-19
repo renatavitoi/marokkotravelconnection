2e85f17a7da49e9518d40069a9cd1a23a50331ad
Renata Vitoi <renata_band@hotmail.com>
2024-06-19 15:05:18 +0200

    Adding home and navbar

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,26 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def tourpackages
+  end
+
+  def experiences
+  end
+
+  def reviews
+  end
+
+  def blog
+  end
+
+  def uberuns
+  end
+
+  def kontakt
+  end
+
+  def faqs
+  end
+
+end
