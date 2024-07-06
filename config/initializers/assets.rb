f96f0e372bba0ae4fa16423dbd5e8b47de65b770
Renata Vitoi <renata_band@hotmail.com>
2024-07-06 11:01:31 +0200

    Correcting the application.rb file and the asset.rb file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "assets","images")
+Rails.application.config.assets.paths << Rails.root.join('app','assets','images')
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -12,5 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join("app", "assets", "asset
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
 
