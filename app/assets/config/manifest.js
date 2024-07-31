369ad04ecd209dde011fc3673ed2ae91d42408e1
Renata Vitoi <renata_band@hotmail.com>
2024-07-31 08:24:37 +0200

    Fix Sprockets ArgumentError

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,7 @@
-
 //= link_tree ../images
 //= link application.js
-//= link application.sass
-//= link engine_app_manifest.js
-//= link_directory ../stylesheets .css
+///= link_directory ../stylesheets .css
+//= link_directory ../javascript .js
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 
