7a5abddc77f3b1aadab1bf33c129b73080a91134
Renata Vitoi <renata_band@hotmail.com>
2024-08-08 23:40:31 +0200

    Fix puma.rb file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,19 +2,24 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('app','assets','images')
-Rails.application.config.assets.precompile += %w( *.js *.css)
+
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'images')
+
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( *.js *.css )
 
 # Example of setting a key with a valid length
 Rails.application.config.assets.key = ENV['ASSETS_KEY']
 Rails.application.credentials.some_key
+
+
 
